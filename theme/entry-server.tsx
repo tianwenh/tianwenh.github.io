92e234d2747003876f80bb698ede816e386815fe
tianwenh <huastronmy@gmail.com>
2022-01-16 16:20:39 -0800

    abstract mdx deps into a package

--- theme/entry-server.tsx
+++ theme/entry-server.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
-import App from '../theme/App';
+import App from './App';
 
 export function render(location: string) {
   return ReactDOMServer.renderToString(
--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mdx from '@mdx-js/rollup';
-import remarkGfm from 'remark-gfm';
-import remarkFrontmatter from 'remark-frontmatter';
-import rehypeSlug from 'rehype-slug';
-import rehypePrism from 'mdx-prism';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import pages from './pages-plugin/plugins/pages';
 import path from 'path';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import pages from './pages-plugin/plugins/pages';
+import mdx from './pages-plugin/plugins/mdx';
 import ssg from './pages-plugin/plugins/ssg';
 
 const distPath = path.resolve('./dist');
 const templatePath = path.resolve(distPath, 'index.html');
-const serverPath = path.resolve('./ssr/entry-server.tsx');
+const serverPath = path.resolve('./theme/entry-server.tsx');
 
 export default defineConfig({
   plugins: [
-    mdx({
-      remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMath],
-      rehypePlugins: [
-        rehypeKatex,
-        rehypePrism,
-        rehypeSlug,
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: { class: 'header-anchor' },
-            content: {
-              type: 'text',
-              value: '#',
-            },
-          },
-        ],
-      ],
-      jsxRuntime: 'classic',
-    }),
-    react(),
+    mdx(),
     pages({
       globs: [
         {
